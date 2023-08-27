9e854006412c73636cf34dd9fda462ac29c15556
IsaacChac <98341379+IsaacChac@users.noreply.github.com>
2023-08-26 18:39:56 -0600

    Update index.s
    Hecho: Ya están listos los comentarios de que tipo de operaciones son cada una y que hace cada bloque :)
    
    Falta: No logré identificar que tipo de operación es CALL ni tampoco que hacen los bloques antes del main.

--- src/index.s
+++ src/index.s
@@ -13,32 +13,32 @@ LC3:
 	.text
 	.globl	_main
 	.def	_main;	.scl	2;	.type	32;	.endef
-_main:
-LFB10:
+_main: ;Aqui se inicia el "main"
+LFB10:;Este bloque corresponde al inicio del switch
 	.cfi_startproc
-	push	ebp
+	push	ebp ; Operación de Transferencia de datos.
 	.cfi_def_cfa_offset 8
 	.cfi_offset 5, -8
-	mov	ebp, esp
+	mov	ebp, esp ; Operación de Transferencia de datos.
 	.cfi_def_cfa_register 5
-	and	esp, -16
-	sub	esp, 32
+	and	esp, -16 ; Operación Lógica
+	sub	esp, 32 ; Operación Aritmética
 	call	___main
-	mov	DWORD PTR [esp+20], 0
-	mov	DWORD PTR [esp+16], 0
-	mov	DWORD PTR [esp+28], 0
-	lea	eax, [esp+24]
-	mov	DWORD PTR [esp+4], eax
-	mov	DWORD PTR [esp], OFFSET FLAT:LC0
+	mov	DWORD PTR [esp+20], 0 ; Operación de Transferencia de datos
+	mov	DWORD PTR [esp+16], 0 ; Operación de Transferencia de datos
+	mov	DWORD PTR [esp+28], 0 ; Operación de Transferencia de datos
+	lea	eax, [esp+24] ; Operación de Transferencia de datos
+	mov	DWORD PTR [esp+4], eax ; Operación de Transferencia de datos
+	mov	DWORD PTR [esp], OFFSET FLAT:LC0 ; Operación de Transferencia de datos
 	call	_scanf
-	mov	eax, DWORD PTR [esp+24]
+	mov	eax, DWORD PTR [esp+24] ; Operación de Transferencia de datos
 	cmp	eax, 5
-	ja	L2
-	mov	eax, DWORD PTR L4[0+eax*4]
-	jmp	eax
+	ja	L2 ; Operación de Control de flujo
+	mov	eax, DWORD PTR L4[0+eax*4] ; Operación de Transferencia de datos
+	jmp	eax ; Operación de Control de flujo
 	.section .rdata,"dr"
 	.align 4
-L4:
+L4: ;Este bloque podría dar los casos dentro del switch
 	.long	L2
 	.long	L3
 	.long	L5
@@ -46,95 +46,95 @@ L4:
 	.long	L7
 	.long	L8
 	.text
-L3:
-	lea	eax, [esp+20]
-	mov	DWORD PTR [esp+4], eax
-	mov	DWORD PTR [esp], OFFSET FLAT:LC0
+L3: ;Este bloque corresponde al "CASE 1 en nuestro Switch"
+	lea	eax, [esp+20] ; Operación de Transferencia de datos
+	mov	DWORD PTR [esp+4], eax ; Operación de Transferencia de datos
+	mov	DWORD PTR [esp], OFFSET FLAT:LC0 ; Operación de Transferencia de datos
 	call	_scanf
-	lea	eax, [esp+16]
-	mov	DWORD PTR [esp+4], eax
-	mov	DWORD PTR [esp], OFFSET FLAT:LC0
+	lea	eax, [esp+16] ; Operación de Transferencia de datos
+	mov	DWORD PTR [esp+4], eax ; Operación de Transferencia de datos
+	mov	DWORD PTR [esp], OFFSET FLAT:LC0 ; Operación de Transferencia de datos
 	call	_scanf
-	mov	edx, DWORD PTR [esp+20]
-	mov	eax, DWORD PTR [esp+16]
-	add	eax, edx
-	mov	DWORD PTR [esp+28], eax
-	mov	eax, DWORD PTR [esp+28]
-	mov	DWORD PTR [esp+4], eax
-	mov	DWORD PTR [esp], OFFSET FLAT:LC0
+	mov	edx, DWORD PTR [esp+20] ; Operación de Transferencia de datos
+	mov	eax, DWORD PTR [esp+16] ; Operación de Transferencia de datos
+	add	eax, edx ; Operación Aritmética
+	mov	DWORD PTR [esp+28], eax ; Operación de Transferencia de datos
+	mov	eax, DWORD PTR [esp+28] ; Operación de Transferencia de datos
+	mov	DWORD PTR [esp+4], eax ; Operación de Transferencia de datos
+	mov	DWORD PTR [esp], OFFSET FLAT:LC0; Operación de Transferencia de datos
 	call	_printf
-	jmp	L9
-L5:
-	lea	eax, [esp+20]
-	mov	DWORD PTR [esp+4], eax
-	mov	DWORD PTR [esp], OFFSET FLAT:LC0
+	jmp	L9; Operación de Control de Flujo
+L5:;Este bloque corresponde al "CASE 2 en nuestro Switch"
+	lea	eax, [esp+20] ; Operación de Transferencia de datos
+	mov	DWORD PTR [esp+4], eax ; Operación de Transferencia de datos
+	mov	DWORD PTR [esp], OFFSET FLAT:LC0 ; Operación de Transferencia de datos
 	call	_scanf
-	lea	eax, [esp+16]
-	mov	DWORD PTR [esp+4], eax
-	mov	DWORD PTR [esp], OFFSET FLAT:LC0
+	lea	eax, [esp+16] ; Operación de Transferencia de datos
+	mov	DWORD PTR [esp+4], eax; Operación de Transferencia de datos
+	mov	DWORD PTR [esp], OFFSET FLAT:LC0; Operación de Transferencia de datos
 	call	_scanf
-	mov	edx, DWORD PTR [esp+20]
-	mov	eax, DWORD PTR [esp+16]
-	sub	edx, eax
-	mov	eax, edx
-	mov	DWORD PTR [esp+28], eax
-	mov	eax, DWORD PTR [esp+28]
-	mov	DWORD PTR [esp+4], eax
-	mov	DWORD PTR [esp], OFFSET FLAT:LC0
+	mov	edx, DWORD PTR [esp+20]; Operación de Transferencia de datos
+	mov	eax, DWORD PTR [esp+16]; Operación de Transferencia de datos
+	sub	edx, eax; Operación Aritmética
+	mov	eax, edx; Operación de Transferencia de datos
+	mov	DWORD PTR [esp+28], eax; Operación de Transferencia de datos
+	mov	eax, DWORD PTR [esp+28]; Operación de Transferencia de datos
+	mov	DWORD PTR [esp+4], eax; Operación de Transferencia de datos
+	mov	DWORD PTR [esp], OFFSET FLAT:LC0; Operación de Transferencia de datos
 	call	_printf
-	jmp	L9
-L6:
-	lea	eax, [esp+20]
-	mov	DWORD PTR [esp+4], eax
-	mov	DWORD PTR [esp], OFFSET FLAT:LC0
+	jmp	L9 ; Operación de Control de flujo
+L6:;Este bloque corresponde al "CASE 3 en nuestro Switch"
+	lea	eax, [esp+20]; Operación de Transferencia de datos
+	mov	DWORD PTR [esp+4], eax; Operación de Transferencia de datos
+	mov	DWORD PTR [esp], OFFSET FLAT:LC0; Operación de Transferencia de datos
 	call	_scanf
-	lea	eax, [esp+16]
-	mov	DWORD PTR [esp+4], eax
-	mov	DWORD PTR [esp], OFFSET FLAT:LC0
+	lea	eax, [esp+16]; Operación de Transferencia de datos
+	mov	DWORD PTR [esp+4], eax; Operación de Transferencia de datos
+	mov	DWORD PTR [esp], OFFSET FLAT:LC0; Operación de Transferencia de datos
 	call	_scanf
-	mov	edx, DWORD PTR [esp+20]
-	mov	eax, DWORD PTR [esp+16]
-	imul	eax, edx
-	mov	DWORD PTR [esp+28], eax
-	mov	eax, DWORD PTR [esp+28]
-	mov	DWORD PTR [esp+4], eax
-	mov	DWORD PTR [esp], OFFSET FLAT:LC1
+	mov	edx, DWORD PTR [esp+20]; Operación de Transferencia de datos
+	mov	eax, DWORD PTR [esp+16]; Operación de Transferencia de datos
+	imul	eax, edx; Operación Aritmética
+	mov	DWORD PTR [esp+28], eax; Operación de Transferencia de datos
+	mov	eax, DWORD PTR [esp+28]; Operación de Transferencia de datos
+	mov	DWORD PTR [esp+4], eax; Operación de Transferencia de datos
+	mov	DWORD PTR [esp], OFFSET FLAT:LC1; Operación de Transferencia de datos
 	call	_printf
-	jmp	L9
-L7:
-	lea	eax, [esp+20]
-	mov	DWORD PTR [esp+4], eax
-	mov	DWORD PTR [esp], OFFSET FLAT:LC0
+	jmp	L9; Operación de Control de flujo
+L7:;Este bloque corresponde al "CASE 4 en nuestro Switch"
+	lea	eax, [esp+20]; Operación de Transferencia de datos
+	mov	DWORD PTR [esp+4], eax; Operación de Transferencia de datos
+	mov	DWORD PTR [esp], OFFSET FLAT:LC0; Operación de Transferencia de datos
 	call	_scanf
-	lea	eax, [esp+16]
-	mov	DWORD PTR [esp+4], eax
-	mov	DWORD PTR [esp], OFFSET FLAT:LC0
+	lea	eax, [esp+16]; Operación de Transferencia de datos
+	mov	DWORD PTR [esp+4], eax; Operación de Transferencia de datos
+	mov	DWORD PTR [esp], OFFSET FLAT:LC0; Operación de Transferencia de datos
 	call	_scanf
-	mov	eax, DWORD PTR [esp+20]
-	mov	ecx, DWORD PTR [esp+16]
+	mov	eax, DWORD PTR [esp+20]; Operación de Transferencia de datos
+	mov	ecx, DWORD PTR [esp+16]; Operación de Transferencia de datos
 	cdq
-	idiv	ecx
-	mov	DWORD PTR [esp+28], eax
-	mov	eax, DWORD PTR [esp+28]
-	mov	DWORD PTR [esp+4], eax
-	mov	DWORD PTR [esp], OFFSET FLAT:LC1
+	idiv	ecx ; Operación Artmética
+	mov	DWORD PTR [esp+28], eax; Operación de Transferencia de datos
+	mov	eax, DWORD PTR [esp+28]; Operación de Transferencia de datos
+	mov	DWORD PTR [esp+4], eax; Operación de Transferencia de datos
+	mov	DWORD PTR [esp], OFFSET FLAT:LC1; Operación de Transferencia de datos
 	call	_printf
-	jmp	L9
-L8:
-	mov	DWORD PTR [esp], OFFSET FLAT:LC2
+	jmp	L9 ; Operación de Control de flujo
+L8:;Este bloque corresponde al "CASE 5 en nuestro Switch"
+	mov	DWORD PTR [esp], OFFSET FLAT:LC2 ; Operación de Transferencia de datos
 	call	_printf
-	jmp	L9
-L2:
-	mov	DWORD PTR [esp], OFFSET FLAT:LC3
+	jmp	L9; Operación de control de flujo
+L2:;Este bloque corresponde al "DEFAULT del Switch"
+	mov	DWORD PTR [esp], OFFSET FLAT:LC3; Operación de Transferencia de datos
 	call	_printf
-L9:
-	mov	eax, 0
+L9:;Este bloque es el que termina el switch y pone el "return 0"
+	mov	eax, 0; Operación de Transferencia de datos
 	leave
 	.cfi_restore 5
 	.cfi_def_cfa 4, 4
 	ret
 	.cfi_endproc
-LFE10:
+LFE10:;En este bloque se da por terminado el main
 	.ident	"GCC: (MinGW.org GCC-6.3.0-1) 6.3.0"
 	.def	_scanf;	.scl	2;	.type	32;	.endef
 	.def	_printf;	.scl	2;	.type	32;	.endef
