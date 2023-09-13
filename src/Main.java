d061d3649875719bef05a4407ba21d8d2d79453c
Victor <89050826+VictorRTXD@users.noreply.github.com>
2023-09-13 01:14:44 +0000

    Main: feat -> abrir y leer p1asm

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 class Main {
     public static void main(String[] args) {
-        System.out.println("hola");
+        String fileName = "P1ASM.asm";
+
+        /*
+        br sirve para leer el contenido del archivo y el while es para leerlo todo usando br.readLIne() y almacenamos cada linea en la variable linea */
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String linea;
+            System.out.println("Contenido original del archivo:");
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea); // Muestra la línea original
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
