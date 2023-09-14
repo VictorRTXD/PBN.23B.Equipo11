4a353976f27e331ab0ca156cd333ab70dc127ec1
IsaacChac <98341379+IsaacChac@users.noreply.github.com>
2023-09-13 19:57:07 -0600

    change variable values
    En lugar de solo imprimir "null" ya le asigné null como un valor real a las etiquetas u operandos cuando corresponda

--- src/testing.java
+++ src/testing.java
@@ -66,26 +66,31 @@ public class testing {
                 
             } else if(matcherCodigoOperacionOperando.matches()) {
                 // Es una línea con código de operación y operando
+                String etiqueta = null;
                 String codigoOperacion = matcherCodigoOperacionOperando.group(1);
                 String operando = matcherCodigoOperacionOperando.group(2);
-                    if(codigoOperacion.length()>5){
-                        System.out.println("Exceso de caracteres en: "+linea);
-                    }else{
-                        System.out.println("Etiqueta: NULL");
-                        System.out.println("Código de Operación: " + codigoOperacion);
-                        System.out.println("Operando/s: " + operando);
-                        System.out.println("");}
+                if(codigoOperacion.length()>5){
+                    System.out.println("Exceso de caracteres en: "+linea);
+                    System.out.println("");
+                }else{
+                System.out.println("Etiqueta: "+etiqueta);
+                System.out.println("Código de Operación: " + codigoOperacion);
+                System.out.println("Operando/s: " + operando);
+                System.out.println("");}
                 
             } else if(matcherCodigoOperacion.matches()) {
                 // Es una línea con código de operación y operando
+                String etiqueta = null;
                 String codigoOperacion = matcherCodigoOperacion.group(1);
+                String operando = null;
                 if(codigoOperacion.length()>5){
                     System.out.println("Exceso de caracteres en: "+linea);
+                    System.out.println("");
                 }else{
-                    System.out.println("Etiqueta: NULL");
-                    System.out.println("Código de Operación: " + codigoOperacion);
-                    System.out.println("Operando/s: NULL");
-                    System.out.println("");}
+                System.out.println("Etiqueta: "+etiqueta);
+                System.out.println("Código de Operación: " + codigoOperacion);
+                System.out.println("Operando/s: "+operando);
+                System.out.println("");}
                 
             } else {
                 // No coincide con ninguna de las expresiones regulares
