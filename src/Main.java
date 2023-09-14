ff49c5cfcfcdc8e83a9eddbcd051a14885680ceb
Victor <89050826+VictorRTXD@users.noreply.github.com>
2023-09-14 02:15:07 +0000

    Ingresando cmabios de testing de Isaac a el Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+//import java.io.BufferedWriter;
 import java.io.FileReader;
-import java.io.FileWriter;
+//import java.io.FileWriter;
 import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -9,7 +9,6 @@ import java.util.regex.Pattern;
 class Main {
     public static void main(String[] args) {
         String fileName = "P1ASM.asm";
-        String lineaCompleta = "";
 
         // ** Expresiones regulares
 
@@ -30,8 +29,6 @@ class Main {
         Pattern patCodigoOperacionOperando = Pattern.compile(regexCodigoOperacionOperando);
         Pattern patCodigoOperacion = Pattern.compile(regexCodigoOperacion);
 
-        
-
         /*
         br sirve para leer el contenido del archivo y el while es para leerlo todo usando br.readLIne() y almacenamos cada linea en la variable linea */
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
@@ -44,36 +41,57 @@ class Main {
                 Matcher matcherCodigoOperacion= patCodigoOperacion.matcher(linea);
     
                 if (matcherComentario.matches()) {
-                    // Es un comentario
-                    if (linea.length() < 80) {
-                        System.out.println("Comentario: " + linea);
+                    if (linea.length () > 80){
+                        System.out.println("Exceso de caracteres en: "+linea);
                     } else {
-                        System.out.println("no sirvo");
-                    }
+                    // Es un comentario
+                        System.out.println("Comentario: " + linea);
+                        System.out.println("");}
                 } else if (matcherCodigo.matches()) {
                     // Es una línea de código con etiqueta, código de operación y operando
                     String etiqueta = matcherCodigo.group(1);
                     String codigoOperacion = matcherCodigo.group(2);
                     String operando = matcherCodigo.group(3);
-    
-                    System.out.println("Etiqueta: " + etiqueta);
-                    System.out.println("Código de Operación: " + codigoOperacion);
-                    System.out.println("Operando: " + operando);
+                        if(etiqueta.length()>8 || codigoOperacion.length()>5){
+                            System.out.println("Exceso de caracteres en: "+linea);
+                        }else{
+                            System.out.println("Etiqueta: " + etiqueta);
+                            System.out.println("Código de Operación: " + codigoOperacion);
+                            System.out.println("Operando: " + operando);
+                            System.out.println("");}
+                    
                 } else if(matcherCodigoOperacionOperando.matches()) {
                     // Es una línea con código de operación y operando
+                    String etiqueta = null;
                     String codigoOperacion = matcherCodigoOperacionOperando.group(1);
                     String operando = matcherCodigoOperacionOperando.group(2);
-    
+                    if(codigoOperacion.length()>5){
+                        System.out.println("Exceso de caracteres en: "+linea);
+                        System.out.println("");
+                    }else{
+                    System.out.println("Etiqueta: "+etiqueta);
                     System.out.println("Código de Operación: " + codigoOperacion);
-                    System.out.println("Operando: " + operando);
+                    System.out.println("Operando/s: " + operando);
+                    System.out.println("");}
+                    
                 } else if(matcherCodigoOperacion.matches()) {
                     // Es una línea con código de operación y operando
+                    String etiqueta = null;
                     String codigoOperacion = matcherCodigoOperacion.group(1);
-    
+                    String operando = null;
+                    if(codigoOperacion.length()>5){
+                        System.out.println("Exceso de caracteres en: "+linea);
+                        System.out.println("");
+                    }else{
+                    System.out.println("Etiqueta: "+etiqueta);
                     System.out.println("Código de Operación: " + codigoOperacion);
+                    System.out.println("Operando/s: "+operando);
+                    System.out.println("");}
+                    
                 } else {
                     // No coincide con ninguna de las expresiones regulares
-                    System.out.println("Línea no reconocida: " + linea);
+                    System.out.println("Error de Sintaxis: " + linea);
+                    System.out.println("");
                 }
             }
         } catch (IOException e) {
