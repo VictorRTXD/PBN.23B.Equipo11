5f25675a358de04962e379bdabf3f4b82e87f4a1
VictorRTXD <victor.macias5736@alumnos.udg.mx>
2023-09-24 13:23:31 -0600

    anadiendo mejoras en excel java

--- MSExcel_In_Java-master/src/excelread/ExcelRead.java
+++ MSExcel_In_Java-master/src/excelread/ExcelRead.java
@@ -7,57 +7,81 @@ package excelread;
 
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.ss.util.CellRangeAddress;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
+import java.util.Iterator;
+import static org.apache.poi.ss.usermodel.CellType.NUMERIC;
+import static org.apache.poi.ss.usermodel.CellType.STRING;
+import org.apache.poi.xssf.usermodel.XSSFFont;
 
 public class ExcelRead {
+
     public static void main(String[] args) {
         try {
-            // Crear un nuevo libro de Excel
-            Workbook workbook = new XSSFWorkbook();
+            // Ruta al archivo de entrada Excel
+            String inputFile = "mi_archivo.xlsx";
+            // Ruta al archivo de salida Excel para documentación
+            String outputFile = "output.xlsx";
 
-            // Crear una nueva hoja en el libro
-            Sheet sheet = workbook.createSheet("MiHoja");
+            FileInputStream fileInputStream = new FileInputStream(new File(inputFile));
+            XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+            XSSFSheet sheet = workbook.getSheetAt(0);
 
-            // Crear una fila
-            Row row = sheet.createRow(0);
+            // Crea un nuevo archivo Excel para la documentación
+            XSSFWorkbook documentationWorkbook = new XSSFWorkbook();
+            XSSFSheet documentationSheet = documentationWorkbook.createSheet("Documentación");
 
-            // Crear una celda y establecer su valor
-            Cell cell = row.createCell(0);
-            cell.setCellValue("Hola, Excel!");
+            int rowIndex = 0;
+            Iterator<Row> rowIterator = sheet.iterator();
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                Iterator<Cell> cellIterator = row.cellIterator();
+                int cellIndex = 0;
+                XSSFRow documentationRow = documentationSheet.createRow(rowIndex);
 
-            // Guardar el libro de Excel en un archivo
-            FileOutputStream fos = new FileOutputStream("mi_archivo.xlsx");
-            workbook.write(fos);
-            fos.close();
-            System.out.println("Archivo Excel creado exitosamente.");
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+                    XSSFCell documentationCell = documentationRow.createCell(cellIndex);
 
-            // Leer el archivo Excel
-            FileInputStream fis = new FileInputStream("mi_archivo.xlsx");
-            workbook = new XSSFWorkbook(fis);
-            Sheet readSheet = workbook.getSheetAt(0);
-            Row readRow = readSheet.getRow(0);
-            Cell readCell = readRow.getCell(0);
-            System.out.println("Valor leído de Excel: " + readCell.getStringCellValue());
+                    // Copiar el valor de la celda del archivo de entrada al archivo de documentación
+                    switch (cell.getCellType()) {
+                        case NUMERIC:
+                            documentationCell.setCellValue(cell.getNumericCellValue());
+                            break;
+                        case STRING:
+                            documentationCell.setCellValue(cell.getStringCellValue());
+                            break;
+                        // Añadir más casos según sea necesario para otros tipos de celdas
+                    }
 
-            // Modificar el valor en Excel
-            readCell.setCellValue("¡Hola, Excel modificado!");
-            fis.close();
+                    // Si deseas aplicar un formato específico a las celdas en el archivo de documentación, puedes hacerlo aquí.
+                    // Por ejemplo, para cambiar el formato de las celdas a negrita:
+                    XSSFCellStyle style = documentationWorkbook.createCellStyle();
+                    XSSFFont font = documentationWorkbook.createFont();
+                    font.setBold(true);
+                    style.setFont(font);
+                    documentationCell.setCellStyle(style);
 
-            // Guardar los cambios
-            FileOutputStream fos2 = new FileOutputStream("mi_archivo_modificado.xlsx");
-            workbook.write(fos2);
-            fos2.close();
-            System.out.println("Archivo Excel modificado y guardado exitosamente.");
+                    cellIndex++;
+                }
 
-        } catch (IOException e) {
+                rowIndex++;
+            }
+
+            // Guarda el archivo de documentación
+            FileOutputStream documentationOutputStream = new FileOutputStream(outputFile);
+            documentationWorkbook.write(documentationOutputStream);
+            documentationOutputStream.close();
+
+            fileInputStream.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
