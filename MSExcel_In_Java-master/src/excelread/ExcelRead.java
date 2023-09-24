912465877b6a775af323b093aedb674184812c0e
VictorRTXD <victor.macias5736@alumnos.udg.mx>
2023-09-24 15:01:06 -0600

    creando excel con archivo pasm

--- MSExcel_In_Java-master/src/excelread/ExcelRead.java
+++ MSExcel_In_Java-master/src/excelread/ExcelRead.java
@@ -16,72 +16,166 @@ import org.apache.poi.ss.util.CellRangeAddress;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Iterator;
 import static org.apache.poi.ss.usermodel.CellType.NUMERIC;
 import static org.apache.poi.ss.usermodel.CellType.STRING;
 import org.apache.poi.xssf.usermodel.XSSFFont;
+import java.io.BufferedReader;
+//import java.io.BufferedWriter;
+import java.io.FileReader;
+//import java.io.FileWriter;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ExcelRead {
 
     public static void main(String[] args) {
         try {
-            // Ruta al archivo de entrada Excel
-            String inputFile = "mi_archivo.xlsx";
-            // Ruta al archivo de salida Excel para documentación
-            String outputFile = "output.xlsx";
+            
+            String fileName = "P1ASM.asm";
+            XSSFWorkbook workbook = new XSSFWorkbook();
 
-            FileInputStream fileInputStream = new FileInputStream(new File(inputFile));
-            XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
-            XSSFSheet sheet = workbook.getSheetAt(0);
+            // Crear una hoja en el libro
+            XSSFSheet sheet = workbook.createSheet("Datos Procesados");
 
-            // Crea un nuevo archivo Excel para la documentación
-            XSSFWorkbook documentationWorkbook = new XSSFWorkbook();
-            XSSFSheet documentationSheet = documentationWorkbook.createSheet("Documentación");
+            // Crear una fila para encabezados
+            XSSFRow headerRow = sheet.createRow(0);
+            XSSFCell headerCell1 = headerRow.createCell(0);
+            headerCell1.setCellValue("ETQ");
+            XSSFCell headerCell2 = headerRow.createCell(1);
+            headerCell2.setCellValue("CODOP");
+            XSSFCell headerCell3 = headerRow.createCell(2);
+            headerCell3.setCellValue("OPR");
+            XSSFCell headerCell4 = headerRow.createCell(3);
+            headerCell4.setCellValue("ADDR");
+            XSSFCell headerCell5 = headerRow.createCell(4);
+            headerCell5.setCellValue("SIZE");
+            
+            
+            int rowNum = 1;
+            
+             // ** Expresiones regulares
 
-            int rowIndex = 0;
-            Iterator<Row> rowIterator = sheet.iterator();
-            while (rowIterator.hasNext()) {
-                Row row = rowIterator.next();
-                Iterator<Cell> cellIterator = row.cellIterator();
-                int cellIndex = 0;
-                XSSFRow documentationRow = documentationSheet.createRow(rowIndex);
+        String regexComentario = "^\\s*;.*$";
 
-                while (cellIterator.hasNext()) {
-                    Cell cell = cellIterator.next();
-                    XSSFCell documentationCell = documentationRow.createCell(cellIndex);
+        // Expresión regular para identificar código de operación y operando
+        String regexCodigoOperacionOperando = "^\\s*([Aa.-Zz]+)\\s+(.*?)$";
 
-                    // Copiar el valor de la celda del archivo de entrada al archivo de documentación
-                    switch (cell.getCellType()) {
-                        case NUMERIC:
-                            documentationCell.setCellValue(cell.getNumericCellValue());
-                            break;
-                        case STRING:
-                            documentationCell.setCellValue(cell.getStringCellValue());
-                            break;
-                        // Añadir más casos según sea necesario para otros tipos de celdas
+        // Expresión regular para identificar etiquetas, códigos de operación y operandos
+        String regexCodigo = "^\\s*([a-zA-Z0-9]+):\\s*([Aa.-Zz]+)\\s+(.*?)$"; 
+
+        //Expresión regular para identificar solo etiqueta y código de operación
+        String regexEtiquetaCodigoOperacion = "^\\s*([a-zA-Z0-9]+):\\s*([Aa.-Zz]+)";
+
+        //Un solo codigo de operacion
+        String regexCodigoOperacion = "^\\s*([Aa.-Zz]+)";
+
+        // Compilar las expresiones regulares
+        Pattern patComentario = Pattern.compile(regexComentario);
+        Pattern patCodigo = Pattern.compile(regexCodigo);
+        Pattern patCodigoOperacionOperando = Pattern.compile(regexCodigoOperacionOperando);
+        Pattern patCodigoOperacion = Pattern.compile(regexCodigoOperacion);
+        Pattern patEtiquetaCodigoOperacion = Pattern.compile(regexEtiquetaCodigoOperacion);
+
+        /*
+        br sirve para leer el contenido del archivo y el while es para leerlo todo usando br.readLIne() y almacenamos cada linea en la variable linea */
+            try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+                String linea;
+                System.out.println("Contenido original del archivo:");
+                while ((linea = br.readLine()) != null) {
+                    Matcher matcherComentario = patComentario.matcher(linea);
+                    Matcher matcherCodigo = patCodigo.matcher(linea);
+                    Matcher matcherCodigoOperacionOperando = patCodigoOperacionOperando.matcher(linea);
+                    Matcher matcherCodigoOperacion= patCodigoOperacion.matcher(linea);
+                    Matcher matcherEtiquetaCodigoOperacion = patEtiquetaCodigoOperacion.matcher(linea);
+
+                    if (matcherComentario.matches()) {
+                        if (linea.length () > 80){
+                            System.out.println("Exceso de caracteres en: "+linea);
+                        } else {
+                        // Es un comentario
+                            System.out.println("Comentario: " + linea);
+                            System.out.println("");}
+                    } else if (matcherCodigo.matches()) {
+                        // Es una línea de código con etiqueta, código de operación y operando
+                        String etiqueta = matcherCodigo.group(1);
+                        String codigoOperacion = matcherCodigo.group(2);
+                        String operando = matcherCodigo.group(3);
+                            if(etiqueta.length()>8 || codigoOperacion.length()>5){
+                                System.out.println("Exceso de caracteres en: "+linea);
+                            }else{
+                                System.out.println("Etiqueta: " + etiqueta);
+                                System.out.println("Código de Operación: " + codigoOperacion);
+                                System.out.println("Operando: " + operando);
+                                System.out.println("");
+                            
+                            XSSFRow row = sheet.createRow(rowNum++);
+                            XSSFCell cell1 = row.createCell(0);
+                            cell1.setCellValue(etiqueta);
+                            XSSFCell cell2 = row.createCell(1);
+                            cell2.setCellValue(codigoOperacion);
+                            XSSFCell cell3 = row.createCell(2);
+                            cell3.setCellValue(operando); 
+                            }
+
+                    } else if(matcherCodigoOperacionOperando.matches()) {
+                        // Es una línea con código de operación y operando
+                        String etiqueta = null;
+                        String codigoOperacion = matcherCodigoOperacionOperando.group(1);
+                        String operando = matcherCodigoOperacionOperando.group(2);
+                        if(codigoOperacion.length()>5){
+                            System.out.println("Exceso de caracteres en: "+linea);
+                            System.out.println("");
+                        }else{
+                        System.out.println("Etiqueta: "+etiqueta);
+                        System.out.println("Código de Operación: " + codigoOperacion);
+                        System.out.println("Operando/s: " + operando);
+                        System.out.println("");}
+
+                    } else if(matcherCodigoOperacion.matches()) {
+                        // Es una línea con código de operación y operando
+                        String etiqueta = null;
+                        String codigoOperacion = matcherCodigoOperacion.group(1);
+                        String operando = null;
+                        if(codigoOperacion.length()>5){
+                            System.out.println("Exceso de caracteres en: "+linea);
+                            System.out.println("");
+                        }else{
+                        System.out.println("Etiqueta: "+etiqueta);
+                        System.out.println("Código de Operación: " + codigoOperacion);
+                        System.out.println("Operando/s: "+operando);
+                        System.out.println("");}
+
+                    }else if(matcherEtiquetaCodigoOperacion.matches()){
+                        String etiqueta = matcherEtiquetaCodigoOperacion.group(1);
+                        String codigoOperacion = matcherEtiquetaCodigoOperacion.group(2);
+                        String operando = null;
+                        if(etiqueta.length()>8 || codigoOperacion.length()>5){
+                            System.out.println("Exceso de caracteres en: "+linea);
+                            System.out.println("");
+                        }else{
+                            System.out.println("Etiqueta: "+etiqueta);
+                            System.out.println("Código de Operación: " + codigoOperacion);
+                            System.out.println("Operando/s: "+operando);
+                            System.out.println("");}
+                        } else {
+                        // No coincide con ninguna de las expresiones regulares
+                        System.out.println("Error de Sintaxis: " + linea);
+                        System.out.println("");
                     }
-
-                    // Si deseas aplicar un formato específico a las celdas en el archivo de documentación, puedes hacerlo aquí.
-                    // Por ejemplo, para cambiar el formato de las celdas a negrita:
-                    XSSFCellStyle style = documentationWorkbook.createCellStyle();
-                    XSSFFont font = documentationWorkbook.createFont();
-                    font.setBold(true);
-                    style.setFont(font);
-                    documentationCell.setCellStyle(style);
-
-                    cellIndex++;
                 }
-
-                rowIndex++;
-            }
-
-            // Guarda el archivo de documentación
-            FileOutputStream documentationOutputStream = new FileOutputStream(outputFile);
-            documentationWorkbook.write(documentationOutputStream);
-            documentationOutputStream.close();
-
-            fileInputStream.close();
-        } catch (Exception e) {
+                try (FileOutputStream fileOut = new FileOutputStream("datos_procesados.xlsx")) {
+                workbook.write(fileOut);
+                }
+                // Cerrar el libro
+                workbook.close();
+                System.out.println("Datos procesados guardados en datos_procesados.xlsx");
+            
+                
+            }  
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
