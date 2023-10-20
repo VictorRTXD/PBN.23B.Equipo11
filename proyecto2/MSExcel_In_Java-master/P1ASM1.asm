38c567b8f4ad314906f0519b124bfcc96f6ccebe
VictorRTXD <victor.macias5736@alumnos.udg.mx>
2023-10-19 18:57:25 -0600

    [p4] (fix) => me falto agregar algunos archivos

---
+++ proyecto2/MSExcel_In_Java-master/P1ASM1.asm
@@ -0,0 +1,66 @@
+- ETQ CODOP OPR
+- ORG $4040
+Et1 SWI -
+Peso total: 2.0
+Direccionamiento: DIR
+dos EORA 5
+Peso total: 2.0
+Direccionamiento: DIR
+- EORA @5
+Peso total: 2.0
+Direccionamiento: DIR
+- EORA %111
+Peso total: 2.0
+Direccionamiento: DIR
+- EORA $6
+Peso total: 3.0
+Direccionamiento: EXT
+- EORA 300
+Peso total: 2.0
+Direccionamiento: DIR
+- EORA $F
+Peso total: 2.0
+Direccionamiento: IMM
+- EORA #5
+Peso total: 2.0
+Direccionamiento: IMM
+- EORA #@5
+- EORA #1500
+Peso total: 2.0
+Direccionamiento: IDX
+- EORA 1,X
+Peso total: 3.0
+Direccionamiento: IDX1
+Uno EORA 255,X
+Peso total: 4.0
+Direccionamiento: IDX2
+- EORA 32768,X
+Peso total: 2.0
+Direccionamiento: IDX
+- EORA $1,+PC
+- EORA A,X
+Peso total: 3.0
+Direccionamiento: IDX1
+- EORA 254,X
+Peso total: 4.0
+Direccionamiento: IDX2
+- EORA 6444,X
+Peso total: 4.0
+Direccionamiento: [IDX2]
+- EORA [1,X]
+Peso total: 4.0
+Direccionamiento: [IDX2]
+- EORA [6444,X]
+Peso total: 2.0
+Direccionamiento: [D,IDX]
+- EORA [D,X]
+Peso total: 2.0
+Direccionamiento: REL
+- BLT UNO
+Peso total: 4.0
+Direccionamiento: REL
+- LBLT UNO
+- IBNE A,UNO
+Tres EQU %111 
+- DS.B %001101010010101011
+- END -
